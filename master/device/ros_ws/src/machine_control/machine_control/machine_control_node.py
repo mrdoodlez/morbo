976856fe079be7ac70be439fb296789720f66a47
mrdoodlez <doodlez1984@gmail.com>
2021-11-15 21:20:53 +0000

    add cmd_vel subscriber

--- master/device/ros_ws/src/machine_control/machine_control/machine_control_node.py
+++ master/device/ros_ws/src/machine_control/machine_control/machine_control_node.py
@@ -2,17 +2,18 @@
 
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 
 class McNode(Node):
     def __init__(self):
         super().__init__("machine_control")
-        self.get_logger().info("hello ros2!")
-        self.counter_ = 0
-        self.create_timer(0.5, self.timer_callback)
+        self.subscriber = self.create_subscription(Twist, "turtle1/cmd_vel",
+                self.twist_callback, 10)
+        self.get_logger().info("machine control node created")
 
-    def timer_callback(self):
-        self.counter_ += 1
-        self.get_logger().info("hello!" + str(self.counter_))
+    def twist_callback(self, msg):
+        self.get_logger().info("lx: " + str(msg.linear.x)
+                + " az: " + str(msg.angular.z))
 
 def main(args=None):
     rclpy.init(args=args)
