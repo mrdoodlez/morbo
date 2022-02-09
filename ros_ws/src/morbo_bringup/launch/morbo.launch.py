0f96298eb431ca401bc8c25c2e1b6f08380e1a5c
mrdoodlez <doodlez1984@gmail.com>
2022-02-09 22:41:24 +0000

    add nav bringup script

--- ros_ws/src/morbo_bringup/launch/morbo.launch.py
+++ ros_ws/src/morbo_bringup/launch/morbo.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
         parameters=[{
             'serial_port': '/dev/ttyUSB1',
             'serial_baudrate': 115200,
-            'frame_id': 'laser',
+            'frame_id': 'base_scan',
             'inverted': False,
             'angle_compensate': True,
         }],
@@ -26,48 +26,27 @@ def generate_launch_description():
 
     pkg_share = launch_ros.substitutions.FindPackageShare(package='morbo_bringup').find('morbo_bringup')
     default_model_path = os.path.join(pkg_share, 'urdf/morbo.urdf')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
+
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-    robot_localization_node = launch_ros.actions.Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[os.path.join(pkg_share, 'config/ekf.yaml'),
-            {'use_sim_time': LaunchConfiguration('use_sim_time')}]
-    )
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
-    )
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-            description='Flag to enable use_sim_time'),
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-            description='Absolute path to rviz config file'),
         diff_drive_node,
         joint_state_publisher_node,
         robot_state_publisher_node,
         rplidar_node,
     ])
-
-    #rviz_node,
