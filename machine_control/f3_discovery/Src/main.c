af748e9ec5e64fce439b4dbfbcb3ca8dca6b6792
mrdoodlez <doodlez1984@gmail.com>
2022-02-15 22:28:03 +0300

    enable i2c for lm3gd20 gyro

--- machine_control/f3_discovery/Src/main.c
+++ machine_control/f3_discovery/Src/main.c
@@ -100,6 +100,20 @@ int main(void) {
 
 	/*************************************************************************/
 
+	__HAL_RCC_GPIOE_CLK_ENABLE();
+
+	GPIO_InitStruct.Pin = GPIO_PIN_3;
+
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+	HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
+
+	/*************************************************************************/
+
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 
 	GPIO_InitStruct.Pin = GPIO_PIN_9;
